34cba8d4e08c59e3aa1f55cb0062de4cf6178ec5
GonzaloMoreiro <gonzalomoreiro@gmail.com>
2025-10-23 17:02:46 -0300

    Instalacion de libreria swagger e implementacion al proyecto

--- back/devcore-back/src/app.module.ts
+++ back/devcore-back/src/app.module.ts
@@ -4,24 +4,24 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import typeOrmConfig from './config/typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [typeOrmConfig]
+      load: [typeOrmConfig],
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
         const typeOrmConfig = configService.get('typeorm');
-        if(!typeOrmConfig) {
+        if (!typeOrmConfig) {
           throw new Error('TypeORM configuration not found');
         }
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
         return typeOrmConfig;
-      }
-    })
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
