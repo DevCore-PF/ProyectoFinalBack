03290ea5dfd98deb2f78ff24870e1595f6253265
Jesus Anaya <j.mtz201289@gmail.com>
2025-10-23 10:19:24 -0600

    Se configura base de datos y se inicializa correctamente

--- back/devcore-back/src/app.module.ts
+++ back/devcore-back/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeOrmConfig from './config/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeOrmConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const typeOrmConfig = configService.get('typeorm');
+        if(!typeOrmConfig) {
+          throw new Error('TypeORM configuration not found');
+        }
+        return typeOrmConfig;
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
