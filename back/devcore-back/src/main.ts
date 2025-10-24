1bb381d9499ec9c76ae2d0560818a52bc14840b6
GonzaloMoreiro <gonzalomoreiro@gmail.com>
2025-10-24 11:57:30 -0300

    Cambios agregados / swagger

--- back/devcore-back/src/main.ts
+++ back/devcore-back/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('devCore')
+    .setDescription(
+      'API construidoa por nest para ser empleada en el ProyectoFinal de la carrera FullStack de Henry',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 
