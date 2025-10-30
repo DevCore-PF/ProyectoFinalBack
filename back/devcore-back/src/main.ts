8f74ab2ecbae808d8b51d875832622d8b6bbc0ec
GonzaloMoreiro <123389608+GonzaloMoreiro@users.noreply.github.com>
2025-10-30 10:53:08 -0300

    Update main.ts

--- back/devcore-back/src/main.ts
+++ back/devcore-back/src/main.ts
@@ -28,7 +28,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
