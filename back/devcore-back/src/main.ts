9ffbcb8669e57da3837832502dc360232f4b7a48
DevCore-PF <devcoreacademia@gmail.com>
2025-10-30 16:55:10 -0600

    Modificacion de cors se agrega url de despliegue de front

--- back/devcore-back/src/main.ts
+++ back/devcore-back/src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://miapp.com'],
+    origin: ['http://localhost:3000', 'https://devcore-front.vercel.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
