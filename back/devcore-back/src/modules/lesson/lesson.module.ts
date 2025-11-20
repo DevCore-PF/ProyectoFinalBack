ce45cbdab67bfe7cc1ce0fcee1a22c9c2b832afc
GonzaloMoreiro <gonzalomoreiro@gmail.com>
2025-11-20 14:55:44 -0300

    Actualicacion, el curso se da en revision despues de dejar contendio adicional en leccion

--- back/devcore-back/src/modules/lesson/lesson.module.ts
+++ back/devcore-back/src/modules/lesson/lesson.module.ts
@@ -5,11 +5,17 @@ import { LessonsController } from './lesson.controller';
 import { Lesson } from './entities/lesson.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CloudinaryModule } from '../cloudinary/cloudinary.module';
+import { CoursesRepository } from '../course/course.repository';
+import { CoursesModule } from '../course/course.module';
 
 @Module({
   controllers: [LessonsController],
   providers: [LessonsService, LessonsRepository],
-  imports: [TypeOrmModule.forFeature([Lesson]), CloudinaryModule],
+  imports: [
+    TypeOrmModule.forFeature([Lesson]),
+    CloudinaryModule,
+    CoursesModule,
+  ],
   exports: [LessonsRepository],
 })
 export class LessonsModule {}
